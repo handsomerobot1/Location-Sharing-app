9bc3935708fed94c0c9714a486047c73ddbecaa7
HandsomeRobot <alaminjoy456@gmail.com>
2024-10-21 13:30:59 +0600

    location view model class created

--- app/src/main/java/com/example/locationsharingapp/FriendsFragment.kt
+++ app/src/main/java/com/example/locationsharingapp/FriendsFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.locationsharingapp.adapter.UserAdapter
 import com.example.locationsharingapp.databinding.FragmentFriendsBinding
+import com.example.locationsharingapp.viewmodel.FireStoreViewModel
 
 
 class FriendsFragment : Fragment() {
 
     private lateinit var binding: FragmentFriendsBinding
-
+    private lateinit var fireStoreViewModel: FireStoreViewModel
+    private lateinit var userAdapter: UserAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
---
+++ app/src/main/java/com/example/locationsharingapp/viewmodel/locationViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.locationsharingapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class locationViewModel : ViewModel() {
+
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+
+}
